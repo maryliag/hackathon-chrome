7238ab26506d9cc94b1798cdb9573bd49e49c41d
Luiz Aoqui <lgfa29@gmail.com>
2013-04-28 22:31:27 -0300

    gerando QRCode usando plugin do jquery

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 var idKey = 'id';
+var server_path = 'ws://localhost:8080';
+// var server_path = 'ws://23.22.100.228:8080';
+var ws;
 
 chrome.app.runtime.onLaunched.addListener(function() {
 	init();
@@ -14,7 +17,47 @@ chrome.runtime.onInstalled.addListener(function() {
 	});
 });
 
-chrome.runtime.onSuspend.addListener(function() { 
-	notification = webkitNotifications.createNotification('', 'Alert!', 'tchaaaaau...');
-	notification.show();
-});
+function init() { 
+	chrome.storage.local.get(idKey, function(items) {
+		var id = items[idKey];
+
+		if (id == null) {
+			id = guid();
+			chrome.storage.local.set({idKey: id});
+		}
+
+		initWebSocket(id);
+	});
+}
+
+function initWebSocket(id) {
+	ws = new WebSocket(server_path);
+
+	ws.onopen = function(event) {
+		console.log(id);
+		ws.send(id);
+	}
+
+	ws.onmessage = function (event) {
+		notification = webkitNotifications.createNotification('', 'Alert!', unescape(event.data));
+		notification.ondisplay = cancelNotification(notification);
+		notification.show();
+	};
+}
+
+setInterval(function() {
+	if (ws != null) {
+		chrome.storage.local.get(idKey, function(items) {
+			var id = items[idKey];
+			ws.send(id);
+		});
+	}
+}, 5000);
+
+function cancelNotification(notification) {
+	if (notification != null) {
+		setTimeout(function() {
+			notification.cancel();
+		}, 3500);
+	}
+}
